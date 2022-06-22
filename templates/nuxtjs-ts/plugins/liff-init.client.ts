2a0f15b785e8976ea2b48a6d747f73549875c7cb
so99ynoodles <so99ynoodles@gmail.com>
2022-06-22 12:47:00 +0900

    fix: ts / lint error

--- templates/nuxtjs-ts/plugins/liff-init.client.ts
+++ templates/nuxtjs-ts/plugins/liff-init.client.ts
@@ -1,28 +1,28 @@
 // This function is executed before instantiating the app
 // only in client-side.
-// document: https://nuxtjs.org/docs/2.x/directory-structure/plugins 
+// document: https://nuxtjs.org/docs/2.x/directory-structure/plugins
 
 // import NPM version LIFF JS SDK
-import liff from '@line/liff';
-import { Plugin } from '@nuxt/types';
+import liff from '@line/liff'
+import { Plugin } from '@nuxt/types'
 
 const liffPlugin: Plugin = (_, inject) => {
   // You can access liff object as this.$liff by inject()
-  inject('liff', liff);
+  inject('liff', liff)
 
   // execute liff.init()
-  const initResult = liff.init({liffId: process.env.LIFF_ID!})
+  const initResult = liff.init({ liffId: process.env.LIFF_ID! })
     .then(() => {
-      console.log('LIFF init succeeded.');
+      console.log('LIFF init succeeded.')
     })
     .catch((error: Error) => {
-      console.log('LIFF init failed.');
-      return Promise.reject(error);
-    });
+      console.log('LIFF init failed.')
+      return Promise.reject(error)
+    })
 
   // You can access liff.init()'s return value (Promise object)
   // as this.$liffInit() by inject()
-  inject('liffInit', initResult);
+  inject('liffInit', initResult)
 }
 
-export default liffPlugin;
+export default liffPlugin
